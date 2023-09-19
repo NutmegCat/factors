f074441e93a3c50e25f69cc9cdc47f45d36b731d
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-09-19 10:48:10 -0400

    method works + In.java

--- factors.java
+++ factors.java
@@ -3,9 +3,17 @@
 class factors {
     public static void main(String[] args) {
         
+        // ask for n 
+        System.out.println("What is your n number?");
+        int n = In.getInt();
+
+        leastFactor(n);
     }
 
-    public static void leastFactor () {
-        
+    public static int leastFactor (int n) {
+
+        System.out.println(n);
+
+        return n;
     }
 }
